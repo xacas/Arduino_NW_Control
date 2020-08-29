17fcacf2038c88379770086b6e60a82fecef9802
xacas <masahiro906@gmail.com>
2020-08-29 22:35:54 +0900

    [Add] main directory and Makefiles

--- Example/Pole_Place/SlipServer.c
+++ Example/Pole_Place/SlipServer.c
@@ -14,60 +14,6 @@
 // Quantize gain
 #define Q_GAIN 25.0
 
-void server(int sockfd);
-
-int main(void){
-
-	int sockfd;
-	int new_sockfd;
-	int writer_len;
-	struct sockaddr_in reader_addr;
-	struct sockaddr_in writer_addr;
-
-	/* make socket */
-	if((sockfd = socket(PF_INET,SOCK_STREAM,0)) < 0){
-		perror("fail at make reader socket");
-		exit(1);
-	}
-	char *opt;
-	opt = "sl0";
-	if(setsockopt(sockfd,SOL_SOCKET,SO_BINDTODEVICE,opt,3) < 0){
-		perror("fail at setsocket opt");
-		exit(1);
-	}
-
-	/* Setting Communication Port, address  */
-	bzero((char *) &reader_addr,sizeof(reader_addr));
-	reader_addr.sin_family=PF_INET;
-	reader_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-	//reader_addr.sin_addr.s_addr=inet_addr("192.168.5.1");
-	reader_addr.sin_port=htons(8000);
-
-	/* bind addres to socket  */
-	if(bind(sockfd, (struct sockaddr *)&reader_addr, sizeof(reader_addr)) < 0){
-		perror("fail at bind socket");
-		exit(1);
-	}
-
-	/* setting conect request */
-	if(listen(sockfd,5)<0){
-		perror("fail at listen");
-		close(sockfd);
-		exit(1);
-	}
-
-	while(1){
-		/* waiting conect request */
-		if((new_sockfd = accept(sockfd,(struct sockaddr *)&writer_addr,&writer_len)) < 0){
-			perror("fail at accept");
-			exit(1);
-		}
-		server(new_sockfd);
-	}
-	close(sockfd);
-	return 0;
-}
-
 char quantizer(float sig){
     return (char)(sig*Q_GAIN);
 }
@@ -86,7 +32,10 @@ void server(int sockfd){
 	float Vo,V1,Vi;
 	FILE *fp;
 
-	read(sockfd,buf,2);
+    if(read(sockfd,buf,2) < 0){
+        close(sockfd);
+        return ;
+    }
 	Vo=dequantizer(buf[0]);
 	V1=dequantizer(buf[1]);
 	Vi=control(Vo,V1);
@@ -94,7 +43,10 @@ void server(int sockfd){
 	printf("%f,%f,%f\n",Vo,V1,Vi);
 
 	write_buf[0]=quantizer(Vi);
-	write(sockfd,write_buf,1);
+    if(write(sockfd,write_buf,1) < 0){
+        close(sockfd);
+        return ;
+    }
 
 	if((fp=fopen("data.csv","a")) != NULL){
 		fprintf(fp,"%f,%f\n",Vo,V1);
---
+++ main/main.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+
+// M系列の更新周期
+#define MSEQ_WIDTH 120
+// Quantize gain
+#define Q_GAIN 25.0
+
+void server(int sockfd);
+
+int main(void){
+
+	int sockfd;
+	int new_sockfd;
+	unsigned int writer_len;
+	struct sockaddr_in reader_addr;
+	struct sockaddr_in writer_addr;
+
+	/* make socket */
+	if((sockfd = socket(PF_INET,SOCK_STREAM,0)) < 0){
+		perror("fail at make reader socket");
+		exit(1);
+	}
+	char *opt;
+	opt = "sl0";
+	if(setsockopt(sockfd,SOL_SOCKET,SO_BINDTODEVICE,opt,3) < 0){
+		perror("fail at setsocket opt");
+		exit(1);
+	}
+
+	/* Setting Communication Port, address  */
+	bzero((char *) &reader_addr,sizeof(reader_addr));
+	reader_addr.sin_family=PF_INET;
+	reader_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	//reader_addr.sin_addr.s_addr=inet_addr("192.168.5.1");
+	reader_addr.sin_port=htons(8000);
+
+	/* bind addres to socket  */
+	if(bind(sockfd, (struct sockaddr *)&reader_addr, sizeof(reader_addr)) < 0){
+		perror("fail at bind socket");
+		exit(1);
+	}
+
+	/* setting conect request */
+	if(listen(sockfd,5)<0){
+		perror("fail at listen");
+		close(sockfd);
+		exit(1);
+	}
+
+	while(1){
+		/* waiting conect request */
+		if((new_sockfd = accept(sockfd,(struct sockaddr *)&writer_addr,&writer_len)) < 0){
+			perror("fail at accept");
+			exit(1);
+		}
+		server(new_sockfd);
+	}
+	close(sockfd);
+	return 0;
+}
