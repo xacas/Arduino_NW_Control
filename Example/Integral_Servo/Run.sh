b4c5f07befbc20b998f4d93bb3af4a04011dc440
xacas <masahiro906@gmail.com>
2020-08-29 17:58:33 +0900

    [Add] Integral Servo Example

---
+++ Example/Integral_Servo/Run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+FILE="data.csv"
+
+if [ -f $FILE ];then
+	rm $FILE
+fi
+
+ps -a | grep slattach | awk '{print $1}' | xargs kill
+
+slattach -L -s 115200 -p slip /dev/ttyUSB0&
+sleep 1s
+ifconfig sl0 192.168.5.1 dstaddr 192.168.5.2
+ifconfig sl0 netmask 255.255.255.0
+./SlipServer
