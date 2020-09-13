91195b86506ad46b1202962cc2a2dca8f692f1d3
MASAHIRO KUSAKA <kusaka@uec.ac.jp>
2020-09-13 18:10:18 +0900

    modify comment for ino files

--- C/example/HelloServer/Hello.ino
+++ C/example/HelloServer/Hello.ino
@@ -11,29 +11,18 @@
 
 // This is the SMTP server to connect to.  This default assumes an SMTP server
 // on your PC.  Note this is a parameter list, so use commas not dots!
-#define SMTP_SERVER 192,168,5,1
+#define HELLO_SERVER 192,168,5,1
 
 // IP and subnet that the Arduino will be using.  Commas again, not dots.
 #define MY_IP 192,168,5,2
 #define MY_SUBNET 255,255,255,0
 
-// If you're using a real Internet SMTP server, the Arduino will need to route
+// If you're using a real Internet server, the Arduino will need to route
 // traffic via your PC, so set the PC's IP address here.  Note that this IP is
 // for the PC end of the SLIP connection (not any other IP your PC might have.)
 // Your PC will have to be configured to share its Internet connection over the
 // SLIP interface for this to work.
 //#define GATEWAY_IP 192,168,5,1
-
-// Uncomment this and adjust the debug() function to print debugging messages
-// somewhere (e.g. an LCD)
-//#define DEBUG
-
-#ifdef DEBUG
-#define debug(x)
-#else
-// If debugging is disabled compile out the messages to save space
-#define debug(x)
-#endif
 
 // Because some of the weird uIP declarations confuse the Arduino IDE, we
 // need to manually declare a couple of functions.
@@ -44,20 +33,19 @@ typedef struct {
   char name[20];
 } connection_data;
 
-uip_ipaddr_t smtp_server;
+uip_ipaddr_t hello_server;
 struct uip_conn *conn;
 #define SET_IP(var, ip)  uip_ipaddr(var, ip)
 bool flag=true;
 
 void setup() {
 
-  // See the HelloWorldServer example for details about this set up procedure.
   Serial.begin(115200);
   SerialIP.use_device(Serial);
   SerialIP.set_uip_callback(uip_callback);
   SerialIP.begin({MY_IP}, {MY_SUBNET});
   pinMode(12, OUTPUT);
-  SET_IP(smtp_server, SMTP_SERVER);
+  SET_IP(hello_server, HELLO_SERVER);
 #ifdef GATEWAY_IP
   SerialIP.set_gateway({GATEWAY_IP});
 #endif
@@ -70,18 +58,12 @@ void setup() {
 void control() {
   
   if(!uip_connected()){
-    conn = uip_connect(&smtp_server, HTONS(8000));
+    conn = uip_connect(&hello_server, HTONS(8000));
     if (conn == NULL) {
       return;
     }
   }
 }
-
-void LED_tick(){
-   digitalWrite(12,HIGH);
-   delay(10);
-   digitalWrite(12,LOW);
-  }
 
 void loop() {
   // Check the serial port and process any incoming data.
