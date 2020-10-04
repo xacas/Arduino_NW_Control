8161a3ee09637439a260a5c077e8761d71493f43
xacas <masahiro906@gmail.com>
2020-10-04 18:38:16 +0900

    [Modify] SignalPlot.jl

--- Julia/SignalPlot.jl
+++ Julia/SignalPlot.jl
@@ -7,36 +7,30 @@ function main()
 
 	# graph setting
 	Title = basename(pwd())
-	OLabel = ["reference" "Vo" "V1"]
+	OLabel = ["V1" "Vo" "reference"]
 	ILabel = [ "Vi" ]
 	ODim = length(OLabel)
 	Dim = ODim+length(ILabel)
 	XLabel = "Time (s)"
 	YLabel = "Voltage (V)"
-	YLimit = (-3,3)
+	YLimit = (-2.5,2.5)
 	# sampling time
 	dt = 0.1
 	# graph width(number of plotting point)
-	width = 300
+	width = 3000
 	# declear x, y
 	x = dt:dt:dt*width
 	y = zeros(width,Dim)
-	x_offset = 0
 
-	for i in 1:2000
-		data = CSV.read("data.csv",header=false)
-		datalen = length(data[!,1])
-		if datalen <= width
-			y[1:datalen,1:Dim] = Matrix(data)
-		else
-			y = Matrix(data[end-(width-1):end,1:Dim])
-			x_offset = dt*(datalen-width)
-		end
-		p1=plot(x + x_offset*ones(width,1),y[1:end,1:ODim],
-		title = Title*" Output",label = OLabel)
-		p2=plot(x + x_offset*ones(width,1),y[1:end,ODim+1:end],
-		title = Title*" Input",label = ILabel[1])
-		display(plot(p1,p2,layout=(2,1),
-		xlabel = XLabel,ylabel = YLabel,ylims = YLimit))
+	data = CSV.read("data.csv",header=false)
+	datalen = length(data[!,1])
+	if datalen <= width
+		y[1:datalen,1:Dim] = Matrix(data)
+	else
+		y = Matrix(data[end-(width-1):end,1:Dim])
 	end
+	p1=plot(x,y[1:end,ODim:-1:1],title = Title*" Output",label = OLabel,lw = 2)
+	p2=plot(x,y[1:end,ODim+1:end],title = Title*" Input",label = ILabel[1],lw = 2)
+	display(plot(p1,p2,layout=(2,1),
+	xlabel = XLabel,ylabel = YLabel,ylims = YLimit))
 end
