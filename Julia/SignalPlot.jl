ff350beb7e941a3927a90d4d0a106e5f807f1c1b
xacas <masahiro906@gmail.com>
2020-10-10 17:55:04 +0900

    [Modify] Plot Setting

--- Julia/SignalPlot.jl
+++ Julia/SignalPlot.jl
@@ -27,10 +27,10 @@ function main()
 	if datalen <= width
 		y[1:datalen,1:Dim] = Matrix(data)
 	else
-		y = Matrix(data[end-(width-1):end,1:Dim])
+		y = Matrix(data[1:width,1:Dim])
 	end
-	p1=plot(x,y[1:end,ODim:-1:1],title = Title*" Output",label = OLabel,lw = 2)
-	p2=plot(x,y[1:end,ODim+1:end],title = Title*" Input",label = ILabel[1],lw = 2)
+	p1=plot(x,y[1:end,ODim:-1:1],title = Title*" Output",label = OLabel,lw = [2 2 1],legend=:bottomleft,line=([:solid :solid :dash]))
+	p2=plot(x,y[1:end,ODim+1:end],title = Title*" Input",label = ILabel[1],lw = 2,legend=:bottomleft)
 	display(plot(p1,p2,layout=(2,1),
 	xlabel = XLabel,ylabel = YLabel,ylims = YLimit))
 end
