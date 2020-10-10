dc2ba3436b150bf5a6cf6134cc3e7885b5f74029
MASAHIRO KUSAKA <kusaka@uec.ac.jp>
2020-10-10 23:29:01 +0900

    [Modify] rename slipserver.c to controlserver.c

--- C/example/Dynamic_Quantizer/FastMode.sh
+++ C/example/Dynamic_Quantizer/FastMode.sh
@@ -15,4 +15,4 @@ slattach -L -s 38400 -p slip /dev/ttyUSB0&
 sleep 1s
 ifconfig sl0 192.168.5.1 dstaddr 192.168.5.2
 ifconfig sl0 netmask 255.255.255.0
-./SlipServer
+./ControlServer
--- C/example/P_Control/StartUp.sh
+++ C/example/P_Control/StartUp.sh
@@ -12,4 +12,4 @@ slattach -L -s 115200 -p slip /dev/ttyUSB0&
 sleep 1s
 ifconfig sl0 192.168.5.1 dstaddr 192.168.5.2
 ifconfig sl0 netmask 255.255.255.0
-./SlipServer
+./ControlServer
